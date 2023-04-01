f377ddd9420d215c44315509bdd7aee19a0d0ba2
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-01 16:13:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Punchlords<title>
+        <title>Punchlords</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
