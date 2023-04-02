515d93914a2ef22dbfe7d066d1f5228c17c9318d
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-01 17:46:51 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
         <Header title="𝐏𝐔𝐍𝐂𝐇𝐋𝐎𝐑𝐃𝐒" />
         <p className="description"> <bold>xn3K28W9eu</bold> is da discord
         </p>
-      <img src="https://is3-ssl.mzstatic.com/image/thumb/Music126/v4/08/d1/a4/08d1a48f-6937-5a67-19ba-131c917b37a7/artwork.jpg/486x486bb.png" alt="This supposed to be a dollar sign" width="100" height="120"></img>
       </main>
 
       <Footer />
