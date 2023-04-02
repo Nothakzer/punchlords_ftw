288c4cfc53f88e0fceedf46022a5d9d072a51b9c
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-01 17:53:19 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="https://live.staticflickr.com/1606/23849528339_03b888a1a1_b.jpg" alt="Dollar Sign" className={styles.logo} />
+        <img src="https://preview.redd.it/p0plbb1l78fa1.png?width=640&crop=smart&auto=webp&v=enabled&s=43896a0b416cf011daeda494799035eb6edbbd61" alt="Dollar Sign" className={styles.logo} />
       </footer>
     </>
   )
