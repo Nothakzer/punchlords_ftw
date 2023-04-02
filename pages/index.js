ed3626a9cfd87ff36638b275b9cd03728321af7d
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-02 13:36:31 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+alert("Succesfully Loaded");
+
 export default function Home() {
   return (
     <div className="container">
