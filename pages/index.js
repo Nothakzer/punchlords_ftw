4d5b248a22dba7dc5dd644cce2aed3f81dd4d6bc
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-01 17:22:07 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="𝐏𝐔𝐍𝐂𝐇𝐋𝐎𝐑𝐃𝐒" />
         <p className="description"> <bold>xn3K28W9eu</bold> is da discord
         </p>
-      <img src="https://www.pngmart.com/files/10/Green-Dollar-Sign-PNG-File.png" alt="This supposed to be a dollar sign" width="500" height="600"></img>
+      <img src="https://www.pngmart.com/files/10/Green-Dollar-Sign-PNG-File.png" alt="This supposed to be a dollar sign" width="100" height="120"></img>
       </main>
 
       <Footer />
