53c614210f3d74586ee5a8ac5703f35a69daf19c
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-04-01 17:27:56 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="www.pngmart.com/files/10/Green-Dollar-Sign-PNG-File.png" alt="Dollar Sign" className={styles.logo} />
+        <img src="https://purepng.com/public/uploads/large/purepng.com-green-dollar-signobjectsgreen-dollar-signmoney-cash-dollar-sign-object-currency-631522323982mojdw.png" alt="Dollar Sign" className={styles.logo} />
       </footer>
     </>
   )
