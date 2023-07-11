919ceb0bcad9ddfdbc2a05eeef77684ffb22b5bb
Psycho_Kinetic <69533714+Nothakzer@users.noreply.github.com>
2023-07-11 18:53:11 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home() {
     
       <main>
         <Header title="𝐋𝐢𝐧𝐤𝐬 𝐂𝐡𝐚𝐩𝐞𝐥​" />
-        <p className="description"> <a href= ""><img src= "https://static-00.iconduck.com/assets.00/telegram-icon-2048x2048-qq8ggh79.png" width= "80" height= "80"></img></a></p> 
+        <p className="description"> <a href= ""><img src= "https://static-00.iconduck.com/assets.00/telegram-icon-2048x2048-qq8ggh79.png" width= "80" height= "80"></img></a></p>
+
+      <p> nordvpn.com/download </p>
       </main>
    
       <Footer />
